0ebffcda27989321f4051ad1acc41f1cb2ef1cd5
Mohnish <mohnish.a007@gmail.com>
2024-03-11 20:06:05 +0000

    Express taking parameters successfully from the URLs

--- index.js
+++ index.js
@@ -8,14 +8,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const mymiddleware = (req,res,next) => {
     console.log(req)
-    //next()
+    //next()   next is used to run the next function or middleware after this middleware
 }
 
-app.use(mymiddleware)
+//app.use(mymiddleware)
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/params/:name', (req, res) => {
+    res.send('Hello World!'+ req.params.name)
+  })
 app.get('/about', (req, res) => {
     //res.sendfile(path.join(__dirname,'index.html' ))
     //res.status(500)
